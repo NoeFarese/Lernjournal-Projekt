633937bae4320c0ab7576056946cec7a27316c4f
NoeFarese <noefarese@gmx.ch>
2023-08-22 16:14:31 +0200

    login gemacht

--- Lernjournal/src/app/login/login.component.ts
+++ Lernjournal/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {RegistrationService} from "../Services/registration.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SnackbarService} from "../Services/snackbar.service";
+import {LoginService} from "../Services/login.service";
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,39 @@ import {RegistrationService} from "../Services/registration.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
+  loginForm: FormGroup;
 
-  constructor(private http: HttpClient, private registrationService: RegistrationService) {}
+  constructor(private formBuilder: FormBuilder, private snackBarService: SnackbarService, private loginService: LoginService) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
   login(): void {
+    if(this.loginForm.invalid) {
+      this.snackBarService.openSnackbar('Email oder Passwort nicht gültig', 'Schliessen', 3000);
+    } else {
+      this.checkIfLoginDataIsCorrect();
+    }
+  }
 
+  private checkIfLoginDataIsCorrect(): void{
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    this.loginService.checkIfUserInputIsValid(email, password).subscribe((exists) => {
+      console.log(exists);
+      if (exists) {
+        this.createLogin(email, password);
+      } else {
+        this.snackBarService.openSnackbar('User existiert nicht', 'Schliessen', 3000);
+      }
+    });
+  }
+
+  private createLogin(email: string, password: string){
+    this.snackBarService.openSnackbar('Du wurdest erfolgreich eingeloggt', 'Schliessen', 3000);
+    this.loginForm.reset();
   }
 }
