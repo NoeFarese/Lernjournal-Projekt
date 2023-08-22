53d5d3d023c041449f607211fcc23c1a89acaa15
NoeFarese <noefarese@gmx.ch>
2023-08-22 16:14:24 +0200

    methoden gelöscht welche nicht gebraucht werden

--- Lernjournal/src/app/Services/registration.service.ts
+++ Lernjournal/src/app/Services/registration.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
-import {catchError, map, Observable, of} from "rxjs";
-import {Registration} from "../Interfaces/Registration";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,37 +8,11 @@ import {Registration} from "../Interfaces/Registration";
 export class RegistrationService {
 
   constructor(private http: HttpClient) {}
+
   private baseUrl = 'http://localhost:8080/registration';
-
-  getRegistrationList(): Observable<any>{
-    return this.http.get(`${this.baseUrl}/all`);
-  }
-
-  getRegistrierung(id: number): Observable<any>{
-    return this.http.get(`${this.baseUrl}/${id}`);
-  }
 
   getEmailExists(email: string): Observable<any>{
     return this.http.get<HttpResponse<any>>(`${this.baseUrl}/${email}/exists`);
-  }
-
-  deleteRegistration(id:number): Observable<Registration> {
-    const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<Registration>(url, this.httpOptions).pipe(
-      catchError(this.handleError<Registration>('deleteEintrag'))
-    );
-  }
-
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      return of(result as T);
-    };
   }
 
   registrateUser(email: string, password: string): Observable<any>{
