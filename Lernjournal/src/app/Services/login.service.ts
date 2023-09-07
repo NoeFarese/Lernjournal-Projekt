46bafb51c09afcb51392aa6eea3752ba77982907
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-09-07 12:09:01 +0200

    methode für das clearen der userEmail

--- Lernjournal/src/app/Services/login.service.ts
+++ Lernjournal/src/app/Services/login.service.ts
@@ -38,4 +38,8 @@ export class LoginService {
   clearAuthorId(): void {
     localStorage.removeItem('authorId');
   }
+
+  clearUserEmail(): void {
+    localStorage.removeItem('userEmail');
+  }
 }
