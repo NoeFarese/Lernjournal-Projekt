28f203f538cd58c8ca40559b91883c3982614d9f
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-09-12 12:55:22 +0200

    Titel auf eine Länge von 40 Zeichen begrenzt

--- micronaut-datenbank/src/main/java/example/micronaut/domain/Eintrag.java
+++ micronaut-datenbank/src/main/java/example/micronaut/domain/Eintrag.java
@@ -1,12 +1,9 @@
 package example.micronaut.domain;
 
 import io.micronaut.serde.annotation.Serdeable;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+
 import static jakarta.persistence.GenerationType.AUTO;
 
 @Serdeable
@@ -19,17 +16,18 @@ public class Eintrag {
     private Long id;
 
     @NotNull
-    @Column(name = "titel", nullable = false, unique = true)
+    @Column(name = "titel", nullable = false, unique = true, length = 40)
     private String titel;
 
-   @NotNull
+    @NotNull
     @Column(name = "text", nullable = true, unique = false, length = 1000000000)
     private String text;
 
     @Column(name = "author_id", nullable = false, unique = false)
     private int author_id;
 
-    public Eintrag() {}
+    public Eintrag() {
+    }
 
     public Eintrag(@NotNull String titel, String text, int author_id) {
         this.titel = titel;
@@ -53,20 +51,28 @@ public class Eintrag {
         this.titel = name;
     }
 
-    public String getText() { return text; }
+    public String getText() {
+        return text;
+    }
 
-    public void setText(String text) { this.text = text; }
+    public void setText(String text) {
+        this.text = text;
+    }
 
-    public void setAuthor_id(int authorId) { this.author_id = authorId; }
+    public void setAuthor_id(int authorId) {
+        this.author_id = authorId;
+    }
 
-    public int getAuthor_id() { return author_id; }
+    public int getAuthor_id() {
+        return author_id;
+    }
 
     @Override
     public String toString() {
         return "Eintrag{" +
-            "id=" + id +
-            ", titel='" + titel +
-            ", text='" + text + '\'' +
-            '}';
+                "id=" + id +
+                ", titel='" + titel +
+                ", text='" + text + '\'' +
+                '}';
     }
 }
