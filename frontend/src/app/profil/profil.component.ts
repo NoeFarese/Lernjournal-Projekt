6ceba236692c9119499f3679d3cd2cf1fe86c42d
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-10-11 10:00:31 +0200

    methode um die anzahl der eintraege zu holen

--- frontend/src/app/profil/profil.component.ts
+++ frontend/src/app/profil/profil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from "../Services/login.service";
+import { ServiceEintrag } from "../Services/service.eintrag";
 
 @Component({
   selector: 'app-profil',
@@ -8,11 +9,22 @@ import { LoginService } from "../Services/login.service";
 })
 export class ProfilComponent implements OnInit{
 
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private eintragService: ServiceEintrag) {}
 
   userEmail: string | null = '';
+  anzahlEintraege: number | null = null;
 
   ngOnInit(){
     this.userEmail = this.loginService.getUserEmail();
+    this.getAnzahlEintraege();
+  }
+
+  getAnzahlEintraege() {
+    const authorId = this.loginService.getAuthorId();
+    if (authorId) {
+      this.eintragService.getAnzahlEintraege(authorId).subscribe(count => {
+        this.anzahlEintraege = count;
+      });
+    }
   }
 }
