e6d879a3fe41f5081c747eb2afba1b1961aaa504
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-10-11 09:14:48 +0200

    userEmail geholt

--- frontend/src/app/profil/profil.component.ts
+++ frontend/src/app/profil/profil.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from "../Services/login.service";
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.css']
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit{
 
+  constructor(private loginService: LoginService) {}
+
+  userEmail: string | null = '';
+
+  ngOnInit(){
+    this.userEmail = this.loginService.getUserEmail();
+  }
 }
