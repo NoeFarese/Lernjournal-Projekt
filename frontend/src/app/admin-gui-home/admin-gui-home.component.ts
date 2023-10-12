ba3dd40a2fcbc65cc11b4754bc2a2dec63c9e405
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-10-12 13:49:46 +0200

    admin-gui-home component erstellt

---
+++ frontend/src/app/admin-gui-home/admin-gui-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-gui-home',
+  templateUrl: './admin-gui-home.component.html',
+  styleUrls: ['./admin-gui-home.component.css']
+})
+export class AdminGuiHomeComponent {
+
+}
---
+++ frontend/src/app/test/admin-gui-home.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminGuiHomeComponent } from '../admin-gui-home/admin-gui-home.component';
+
+describe('AdminGuiHomeComponent', () => {
+  let component: AdminGuiHomeComponent;
+  let fixture: ComponentFixture<AdminGuiHomeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AdminGuiHomeComponent]
+    });
+    fixture = TestBed.createComponent(AdminGuiHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
