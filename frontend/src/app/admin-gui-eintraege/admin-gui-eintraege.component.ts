8971add1e927b0d4fbf1ea2f57e752467d662bae
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-10-13 07:04:39 +0200

    admin-gui-eintraege component erstellt

---
+++ frontend/src/app/admin-gui-eintraege/admin-gui-eintraege.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-gui-eintraege',
+  templateUrl: './admin-gui-eintraege.component.html',
+  styleUrls: ['./admin-gui-eintraege.component.css']
+})
+export class AdminGuiEintraegeComponent {
+
+}
---
+++ frontend/src/app/test/admin-gui-eintraege.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminGuiEintraegeComponent } from '../admin-gui-eintraege/admin-gui-eintraege.component';
+
+describe('AdminGuiEintraegeComponent', () => {
+  let component: AdminGuiEintraegeComponent;
+  let fixture: ComponentFixture<AdminGuiEintraegeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AdminGuiEintraegeComponent]
+    });
+    fixture = TestBed.createComponent(AdminGuiEintraegeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
