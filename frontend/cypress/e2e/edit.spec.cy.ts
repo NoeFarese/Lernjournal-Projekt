2f8f014d539b2260ee2242204802cba0a36c8576
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-11-15 10:25:04 +0100

    edit tests fertig gemacht

--- frontend/cypress/e2e/edit.spec.cy.ts
+++ frontend/cypress/e2e/edit.spec.cy.ts
@@ -19,21 +19,6 @@ describe('Edit Page Test', () => {
         cy.get('.exportPdf-button').should('exist');
     });
 
-/*
-    it('should insert text with format when button clicked', () => {
-        const text = 'This is a test.';
-        cy.get('#text')
-            .type(text)
-            .select(5, 10)
-            .then(() => {
-                cy.get('.editor-menubar button[matTooltip="Fett"]').click();
-            });
-
-        cy.get('#text').should('have.value', 'This <b>is a</b> test.');
-    });
- */
-
-
     it('should save the entry when save button clicked', () => {
         const titel = 'Test Titel';
         const text = 'This is a test entry.';
@@ -51,5 +36,35 @@ describe('Edit Page Test', () => {
         cy.get('#text').type(text);
         cy.get('.save-button').click();
         cy.contains('Ein Eintrag mit diesem Titel existiert bereits').should('exist');
+    });
+
+    it('should write text in textarea and apply bold formatting', () => {
+        const inputText = 'This is a test.';
+        const titleText = 'Test Title';
+        cy.get('#titel').type(titleText);
+        cy.get('#text').type(inputText);
+        cy.get('#text').invoke('prop', 'selectionStart', 0).invoke('prop', 'selectionEnd', inputText.length);
+        cy.get('.editor-menubar button[matTooltip="Fett"]').should('exist').click();
+        cy.get('#text').should('have.value', `<b>This is a test.</b>`);
+    });
+
+    it('should write text in textarea and apply italic formatting', () => {
+        const inputText = 'This is a test.';
+        const titleText = 'Test Title';
+        cy.get('#titel').type(titleText);
+        cy.get('#text').type(inputText);
+        cy.get('#text').invoke('prop', 'selectionStart', 0).invoke('prop', 'selectionEnd', inputText.length);
+        cy.get('.editor-menubar button[matTooltip="Kursiv"]').should('exist').click();
+        cy.get('#text').should('have.value', `<i>This is a test.</i>`);
+    });
+
+    it('should write text in textarea and apply title formatting', () => {
+        const inputText = 'This is a test.';
+        const titleText = 'Test Title';
+        cy.get('#titel').type(titleText);
+        cy.get('#text').type(inputText);
+        cy.get('#text').invoke('prop', 'selectionStart', 0).invoke('prop', 'selectionEnd', inputText.length);
+        cy.get('.editor-menubar button[matTooltip="Titel"]').should('exist').click();
+        cy.get('#text').should('have.value', `<h1>This is a test.</h1>`);
     });
 });
