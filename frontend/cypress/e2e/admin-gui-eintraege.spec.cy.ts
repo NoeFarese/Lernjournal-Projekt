32d5a307df3502edea3fd79898ac4228e2b407e5
Noé <noefarese@gmx.ch>
2023-11-16 12:05:34 +0100

    tests für admin-gui-eintraege component gemacht

---
+++ frontend/cypress/e2e/admin-gui-eintraege.spec.cy.ts
@@ -0,0 +1,28 @@
+describe('Admin-GUI-Home Page Test', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:80/login');
+        cy.get('input[formControlName="email"]').type('test@css.ch');
+        cy.get('input[formControlName="password"]').type('testcss');
+        cy.get('button[type="submit"]').click();
+
+        cy.url().should('eq', 'http://localhost/home');
+
+        cy.visit('http://localhost/admin/eintraege/b@b.ch');
+        cy.url().should('eq', 'http://localhost/admin/eintraege/b@b.ch');
+    });
+
+    it('should display user email in the heading', () => {
+        cy.get('h1 b').should('have.text', 'b@b.ch');
+    });
+
+    it('should filter entries by search term', () => {
+        const searchTerm = '2';
+        cy.get('mat-form-field input').type(searchTerm, { force: true });
+        cy.get('.eintrag-item a').should('contain', searchTerm);
+    });
+
+    it('should click first entry', () => {
+        cy.get('.eintrag-item').first().find('a').click();
+        cy.url().should('include', '/eintrag/');
+    });
+});
