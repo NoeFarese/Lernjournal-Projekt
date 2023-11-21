1f4b9f0e681f947d17db8eb36f98223b26dce62d
Noé <110814367+NoeFarese@users.noreply.github.com>
2023-11-21 15:27:30 +0100

    registrate, deleteUser and clearForm methode hinzugefügt

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -4,6 +4,9 @@ declare namespace Cypress {
     interface Chainable {
         getMatSnackbar(): Chainable<string>;
         login(email: string, password: string): Chainable;
+        registrate(email: string, password: string, confirmPassword: string): Chainable;
+        deleteUser(email: string, password: string, confirmPassword: string):Chainable;
+        clearForm(): Chainable;
     }
 }
 
@@ -12,8 +15,29 @@ function getMatSnackbar(): Chainable<string> {
 }
 
 Cypress.Commands.add('getMatSnackbar', getMatSnackbar);
-Cypress.Commands.add('login', (email: any, password: any) => {
+
+Cypress.Commands.add('login', (email: string, password: string) => {
     cy.get('input[formControlName="email"]').type(email);
     cy.get('input[formControlName="password"]').type(password);
     cy.get('button[type="submit"]').click();
+});
+
+Cypress.Commands.add('registrate', (email: string, password: string, confirmPassword: string) => {
+    cy.get('[formControlName="email"]').type(email);
+    cy.get('[formControlName="password"]').type(password);
+    cy.get('[formControlName="confirmPassword"]').type(confirmPassword, { force: true });
+    cy.get('form').submit();
+});
+
+Cypress.Commands.add('deleteUser', (email: string, password: string, confirmPassword: string) => {
+    cy.get('[formControlName="email"]').type(email);
+    cy.get('[formControlName="password"]').type(password);
+    cy.get('[formControlName="confirmPassword"]').type(confirmPassword, { force: true });
+    cy.get('.delete-account-button').click();
+});
+
+Cypress.Commands.add('clearForm', () => {
+    cy.get('[formControlName="email"]').clear();
+    cy.get('[formControlName="password"]').clear();
+    cy.get('[formControlName="confirmPassword"]').clear();
 });
