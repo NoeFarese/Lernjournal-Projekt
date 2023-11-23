1f5b36d0403e77aaec008d6a87a068da4a3890f1
Noé <noefarese@gmx.ch>
2023-11-23 09:14:29 +0100

    eintrag tests angepasst

--- frontend/cypress/e2e/eintrag.spec.cy.ts
+++ frontend/cypress/e2e/eintrag.spec.cy.ts
@@ -1,17 +1,20 @@
 describe('Eintrag Test', () => {
     beforeEach(() => {
+        cy.visit('http://localhost/login');
+        cy.fillOutLoginForm('test@css.ch', 'testcss');
+
         cy.visit('http://localhost/eintrag/42');
     });
 
     it('should check if HTML elements are existing', () => {
         cy.get('.eintrag-titel').should('exist');
         cy.get('.eintrag-text').should('exist');
-        cy.get('.redirect-to-home-button').should('exist');
+        cy.get('.go-back-button').should('exist');
     });
 
-    it('should check the "zur Homeseite" button', () => {
-        cy.get('.redirect-to-home-button').should('have.text', 'Zur Homeseite');
-        cy.get('.redirect-to-home-button').click();
+    it('should check the "go back" button', () => {
+        cy.get('.go-back-button').should('have.text', 'go back');
+        cy.get('.go-back-button').click();
 
         cy.url().should('eq', 'http://localhost/home');
     });
@@ -19,13 +22,13 @@ describe('Eintrag Test', () => {
     it('should have the correct styling for elements', () => {
         cy.get('.eintrag-titel').should('have.css', 'font-size', '24px');
         cy.get('.eintrag-text').should('have.css', 'color', 'rgb(51, 51, 51)');
-        cy.get('.redirect-to-home-button').should('have.css', 'background-color', 'rgb(64, 59, 184)');
+        cy.get('.go-back-button').should('have.css', 'background-color', 'rgb(64, 59, 184)');
     });
 
     it('should be visible after data is loaded', () => {
         cy.get('.eintrag-titel').should('be.visible');
         cy.get('.eintrag-text').should('be.visible');
-        cy.get('.redirect-to-home-button').should('be.visible');
+        cy.get('.go-back-button').should('be.visible');
     });
 });
 
